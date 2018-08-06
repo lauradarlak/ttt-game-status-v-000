3e31422c0ddb57f2bbf8be4d732c902f33793b07
Laura Darlak <laura@polyporedesign.com>
2018-08-06 16:58:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -63,7 +63,11 @@ def winner(board)
     board[win_combination[1]] == "X" &&
     board[win_combination[2]] == "X"
       return "X"
-    else return "O"
+    elsif board[win_combination[0]] == "O" &&
+    board[win_combination[1]] == "O" &&
+    board[win_combination[2]] == "O"
+      return "O"
+    else return nil
     end
   end
 end
